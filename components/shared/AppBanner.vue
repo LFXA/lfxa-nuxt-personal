44553aa1c6c4e3a96a143999506e6cdc193d982a
LucasFelixAquino <ext.lucas.aquino@farfetch.com>
2023-12-22 13:58:33 -0300

    Add CV

--- components/shared/AppBanner.vue
+++ components/shared/AppBanner.vue
@@ -62,7 +62,7 @@ export default {
       </p>
       <div class="flex justify-center sm:block">
         <a
-          :href="$i18n.locale == 'pt-BR' ?'/Stoman-Resume.pdf':'/Stoman-ResumeEN.pdf'"
+          :href="$i18n.locale == 'pt-BR' ?'/lfxa-pt.pdf':'/lfxa-en.pdf'"
           download=""
           class="
             flex
